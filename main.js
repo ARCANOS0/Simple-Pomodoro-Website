8496944ac00be223fbe43f4e201edf38779dc231
ARCANOS0 <Tarek2019MaRco@gmail.com>
2025-01-18 16:57:55 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+const start = document.getElementById("start");
+const stop = document.getElementById("stop");
+const reset = document.getElementById("reset");
+const timer = document.getElementById("timer");
+
+
+
+let timeLeft = 3000;
+let intervallo; 
+
+const updateTimer = () => {
+
+  const minutes = Math.floor(timeLeft / 60);
+  const seconds = timeLeft % 60;
+
+  timer.innerHTML = `${minutes.toString().padStart(2,"0")}
+  :
+  ${seconds.toString().padStart(2,"0")}`;
+};
+
+// start button action
+const startTimer = () => {
+  intervallo = setInterval(() => {
+    timeLeft--;
+    updateTimer();
+    if (timeLeft === 0){
+      clearInterval(intervallo);
+      alert("Time is up!!!");
+      timeLeft = 3000;
+      updateTimer();
+    }
+  }, 1000)
+};
+
+
+// stop button action
+const stopTimer = () => clearInterval(intervallo);
+
+// reset button action
+const resetTimer = () => {
+  clearInterval(intervallo);
+  timeLeft = 3000;
+  updateTimer();
+}
+
+
+start.addEventListener('click', startTimer);
+stop.addEventListener('click', stopTimer);
+reset.addEventListener('click', resetTimer);
